32e4e74e572e794f7a0a3fff9a4710faa3e19698
Duterte Fabien <fabien_duterte@ens.univ-artois.fr>
2019-09-30 12:29:17 +0200

    handle git config

---
+++ config/config-exemple.php
@@ -0,0 +1,19 @@
+<?php
+
+use Hypario\Database\DatabaseFactory;
+use Hypario\Renderer\PHPRenderer;
+use Hypario\Renderer\RendererInterface;
+use function DI\factory;
+
+return [
+
+    'database.host' => 'localhost',
+    'database.username' => 'root',
+    'database.password' => 'root',
+    'database.name' => 'stormbox',
+    'database.schema' => 'stormbox',
+
+    RendererInterface::class => \DI\create(PHPRenderer::class)->constructor(ROOT . '/views'),
+
+    PDO::class => factory(DatabaseFactory::class),
+];
